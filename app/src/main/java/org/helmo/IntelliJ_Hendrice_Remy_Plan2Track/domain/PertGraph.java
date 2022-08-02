7d1694ab37ab7152d5b1b3ede30402a004da3a96
E190449 <r.hendrice@student.helmo.com>
2022-08-02 20:03:08 +0200

    Us-4: Pert en cours - Calcul des niveaux - avec currentLevel

--- app/src/main/java/org/helmo/IntelliJ_Hendrice_Remy_Plan2Track/domain/PertGraph.java
+++ app/src/main/java/org/helmo/IntelliJ_Hendrice_Remy_Plan2Track/domain/PertGraph.java
@@ -15,15 +15,15 @@ public class PertGraph {
 
     private void calcEdgeLevels(List<Job> jobs){
         var priorMatrix = initPriorMatrix(jobs);
-        int[] levels = new int[jobs.size()];
-        for (int i = 0; i < jobs.size(); ++i) {
-            levels[i] = sum(priorMatrix[i]);
+        var levelsPerJob = new int[jobs.size()];
+        int currentLevel = 0;
+
+        while(!jobs.isEmpty()){
+            Set<PertEdge> level = new HashSet<>();
+            determineLevels(jobs, level, levelsPerJob, priorMatrix, currentLevel);
+            edgeLevels.add(level);
+            currentLevel++;
         }
-
-    }
-
-    private int sum(int[] row){
-        return Arrays.stream(row).sum();
     }
 
     private int[][] initPriorMatrix(List<Job> jobs){
@@ -32,15 +32,44 @@ public class PertGraph {
         for(int[] row : priorMatrix){
             Arrays.fill(row, 0);
         }
-        for(int prior = 0; prior < dim; ++prior ){
-            for(int after = 0; after < dim; ++after){
-                if(jobs.get(after).hasPrior(jobs.get(prior))){
-                    priorMatrix[after][prior] = 1;
+        for(int hasToBeDone = 0; hasToBeDone < dim; ++hasToBeDone ){
+            for(int toDo = 0; toDo < dim; ++toDo){
+                if(jobs.get(toDo).hasPrior(jobs.get(hasToBeDone))){
+                    priorMatrix[toDo][hasToBeDone] = 1;
                 }
             }
         }
         return priorMatrix;
     }
 
+    private void determineLevels(List<Job> jobs, Set<PertEdge> level,
+                                 int[] levelsPerJob, int[][] priorMatrix, int currentLevel){
+        sumOnesInRow(levelsPerJob, priorMatrix);
+        clearZerosRows(jobs, level, levelsPerJob, priorMatrix, currentLevel);
+    }
 
+    private void sumOnesInRow(int[] levels, int[][] priorMatrix){
+        for (int i = 0; i < levels.length; ++i) {
+            levels[i] = sum(priorMatrix[i]);
+        }
+    }
+
+    private int sum(int[] row){
+        return Arrays.stream(row).sum();
+    }
+
+    private void clearZerosRows(List<Job> jobs, Set<PertEdge> level,
+                                int[] levelsPerJob, int[][] priorMatrix, int currentLevel){
+        for (int i = 0; i < jobs.size(); ++i) {
+            if(levelsPerJob[i] == 0){
+                clearColumn(i, priorMatrix);
+            }
+        }
+    }
+
+    private void clearColumn(int col, int[][] matrix){
+        for(int i = 0; i < matrix.length; ++i){
+            matrix[i][col] = 0;
+        }
+    }
 }
